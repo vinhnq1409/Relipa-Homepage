fe1a0ebd7f45e63d22c07e0e738aa7893152d930
Dang Trong Truong <dangtrongtruong190197@gmail.com>
2022-04-04 21:33:11 +0700

    feat: login page ui

--- pages/admin/signin.js
+++ pages/admin/signin.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
@@ -7,7 +6,6 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
@@ -43,16 +41,15 @@ export default function SignIn() {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    Router.push("/admin/dashboard");
+  };
 
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
+        <Typography component="h1" color="primary" variant="h2">
           Relipa
         </Typography>
         <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
@@ -64,16 +61,15 @@ export default function SignIn() {
             id="email"
             label="Email Address"
             name="email"
-            {...register('email', {
-              required:true,
+            {...register("email", {
+              required: true,
               pattern: {
-                  value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                  message: 'Please enter a valid email',
+                value:
+                  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                message: "Please enter a valid email",
               },
-          })}
-            autoComplete="email"
+            })}
             type="email"
-            autoFocus
           />
           <TextField
             variant="outlined"
@@ -85,7 +81,6 @@ export default function SignIn() {
             label="Password"
             type="password"
             id="password"
-            autoComplete="current-password"
           />
           <FormControlLabel
             control={<Checkbox value="remember" color="primary" />}
--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,20 @@ import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 export default function Index() {
-  
   const router = useRouter();
-  const [authorized,setAuthorized] = useState(false)
+  const [authorized, setAuthorized] = useState(false);
 
-  useEffect(()=>{
-    if(!authorized){
+  useEffect(() => {
+    if (!authorized) {
       router.push({
-        pathname:"/signin"
-      })
-    }
-    else{
+        pathname: "/admin/signin",
+      });
+    } else {
       router.push({
-        pathname:"/admin/dashboard"
-      })
+        pathname: "/admin/dashboard",
+      });
     }
-  },[authorized])
+  }, [authorized]);
 
   return <div />;
 }
