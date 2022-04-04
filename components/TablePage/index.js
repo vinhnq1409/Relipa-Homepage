96dabc11fb1a3877c84ed551a8bbec79d7573108
Thanhpt <thanhpt@relipasoft.com>
2022-04-04 22:07:13 +0700

    update

--- components/TablePage/index.js
+++ components/TablePage/index.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { LinearProgress } from "@material-ui/core";
+import { Skeleton } from "@material-ui/lab";
 import { TableList } from "./componentTable/tableList";
 import { CommonTable } from "./componentTable/CommonTable";
 
 export const RenderPage = () => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  });
+
   const headerTable = [
     { id: "no", isSort: false, label: "No" },
     { id: "subject", isSort: false, label: "Subject" },
@@ -31,15 +40,30 @@ export const RenderPage = () => {
       subject: "Chính sách bảo hiểm 2022",
       author: "Nguyễn Văn An",
     },
+    {
+      id: 10,
+      subject: "An toàn công nghệ thông tin",
+      author: "Vũ Văn Vịnh",
+    },
   ];
-  
+
   return (
     <>
-      <CommonTable headCells={headerTable}>
+      {loading ? (
+        <div>
+          <LinearProgress />
+          <Skeleton variant="text" height={80}/>
+          {data.map((item, index) => (
+          <Skeleton variant="text" height={40}/>
+        ))}
+        </div>
+      ) : (
+        <CommonTable headCells={headerTable}>
         {data.map((item, index) => (
           <TableList key={index} id={index} item={item} />
         ))}
       </CommonTable>
+      )}
     </>
   );
 };
