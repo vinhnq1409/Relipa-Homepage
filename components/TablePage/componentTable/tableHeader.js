e87cea1518a6dbd7e5b5dbf8e430092a5682aa48
thanhpt <thanhpt@relipasoft.com>
2022-04-07 08:11:13 +0700

    conflix

--- components/TablePage/componentTable/tableHeader.js
+++ components/TablePage/componentTable/tableHeader.js
@@ -1,22 +1,19 @@
 import React from "react";
-import { TableHead, TableRow, TableSortLabel, TableCell } from "@material-ui/core";
+import {
+  TableHead,
+  TableRow,
+  TableSortLabel,
+  TableCell,
+} from "@material-ui/core";
 
-export const EnhancedTableHead = (props) => {
-  const {headCells} = props
-
+export const EnhancedTableHead = ({ headCells }) => {
   return (
     <TableHead>
       <TableRow>
-        {headCells.map(headCell => {
-          <TableCell key={headCell.id}>
-            <TableSortLabel
-              hideSortIcon = {true}
-            >
-              {headCell.label}
-            </TableSortLabel>
-          </TableCell>
-        })}
+        {headCells.map((headCell) => (
+          <TableCell key={headCell.id}>{headCell.label}</TableCell>
+        ))}
       </TableRow>
     </TableHead>
-  )
-}
+  );
+};
