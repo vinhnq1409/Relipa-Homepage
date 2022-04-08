128aa624f0e00db1a59b244826774c5dee8bd1f3
tungvt <tungvt@relipasoft.com>
2022-04-08 11:28:57 +0700

    fix conflic

--- redux/reducers.js
+++ redux/reducers.js
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import demoSlice from './slices/demoSlice'
 import infoUserSlice from './slices/userInfo'
+import blogSlice from './slices/blogSlice'
 
 export default combineReducers({
   userInfo: infoUserSlice,
   demo: demoSlice,
+  blog: blogSlice
 })
