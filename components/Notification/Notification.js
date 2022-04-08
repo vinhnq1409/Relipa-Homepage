097e91d558214c4bcb233c9551512717862627ab
vinhnq99 <vinhnq@relipasoft.com>
2022-04-08 16:30:38 +0700

    notification

---
+++ components/Notification/Notification.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Snackbar } from '@material-ui/core'
+import MuiAlert from "@material-ui/lab/Alert";
+
+
+
+export default function Notification({open,handleClose}) {
+  return <>
+        <Snackbar anchorOrigin={{ vertical: 'top', horizontal: 'right' }} open={open.notification} autoHideDuration={2000} onClose={handleClose}>
+        <MuiAlert
+          elevation={6}
+          variant="filled"
+          onClose={handleClose}
+          severity={open.severity}
+          >
+            {open.title}
+        </MuiAlert>
+        </Snackbar>
+  </>
+}
