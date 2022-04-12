a4a6403e1dac2c0ad2ed1366822ec9e4accff1c1
thanhpt <thanhpt@relipasoft.com>
2022-04-12 09:41:35 +0700

    conflix

--- pages/admin/account/index.js
+++ pages/admin/account/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Admin from 'layouts/Admin.js'
-<<<<<<< HEAD
 import { Button } from '@material-ui/core'
 import { dataUJson, headerUJson } from '../../../sampleData/userInfo'
 import TableListAdmin from './tableAdmin'
@@ -14,23 +13,13 @@ export default function Account() {
   const changeURL = () => {
     router.push({ pathname: 'account/add', query: { slug: 'about', mode: 'add' }})
   }
+
   return (
     <>
       <div className= {style.dFlex}>
         <Button className= {style.buttonLeft} onClick={changeURL} color = 'primary' variant = 'contained'>Create Account</Button>
       </div>
       <TableListAdmin
-=======
-import TableList from '../blogs/TableList'
-import { dataUJson, headerUJson } from '../../../sampleData/userInfo'
-
-export default function Account() {
-  const dataUse = JSON.parse(dataUJson)
-  const tableHead = JSON.parse(headerUJson)
-  return (
-    <>
-      <TableList
->>>>>>> 73757c3 (test version)
         data={dataUse}
         tableHead = {tableHead}
       />
