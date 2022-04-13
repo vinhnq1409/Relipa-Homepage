f6c7882608701daea502bd9a7e1616ffefe4e587
thanhpt <thanhpt@relipasoft.com>
2022-04-13 11:44:08 +0700

    conflix

---
+++ pages/admin/static-page.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { RenderPage } from "../../components/TablePage";
+import Admin from "layouts/Admin.js";
+
+
+export default function StaticPage() {
+  return <RenderPage />;
+}
+
+StaticPage.layout = Admin;
