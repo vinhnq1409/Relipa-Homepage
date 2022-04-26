c0cf55f6b786a64371f5591e1708712e60c1e5fe
tructh <tructh@relipasoft.com>
2022-04-26 14:46:28 +0700

    Fix import js

--- layouts/Home.js
+++ layouts/Home.js
@@ -1,11 +1,6 @@
 import React from 'react'
-
 import Footer from '../components/Footer/HomeFooter'
 import Header from '../components/Header/Header'
-
-// import '../public/user-page/css/aos.css'
-
-import Script from 'next/script'
 import Head from 'next/head'
 
 export default function Home({ children, ...rest }) {
@@ -13,7 +8,7 @@ export default function Home({ children, ...rest }) {
     <>
       <Head>
         <link rel='stylesheet' href='/user-page/css/line-awesome.min.css' />
-        {/* <link rel='stylesheet' href='/user-page/css/aos.css' /> */}
+        <link rel='stylesheet' href='/user-page/css/aos.css' />
         <link rel='stylesheet' href='/user-page/css/swiper-bundle.min.css' />
         <link rel='stylesheet' href='/user-page/css/template.css' />
       </Head>
@@ -21,15 +16,15 @@ export default function Home({ children, ...rest }) {
         <Header />
         <div>{children}</div>
         <Footer />
-        <Script src='/user-page/js/popper.min.js' />
-        <Script src='/user-page/js/bootstrap.js' />
+        <script src='/user-page/js/popper.min.js' />
+        <script src='/user-page/js/bootstrap.js' />
 
-        <Script src='/user-page/js/isotope.pkgd.min.js' />
-        <Script src='/user-page/js/swiper-bundle.min.js' />
-        <Script src='/user-page/js/aos.js' />
+        <script src='/user-page/js/isotope.pkgd.min.js' />
+        <script src='/user-page/js/swiper-bundle.min.js' />
+        <script src='/user-page/js/aos.js' />
 
-        <Script src='/user-page/js/main.js' />
-        <Script src='/user-page/js/isotop-filter.js' />
+        <script src='/user-page/js/main.js' />
+        <script src='/user-page/js/isotop-filter.js' />
       </div>
     </>
   )
