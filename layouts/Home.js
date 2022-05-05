8459857f594d6868823f5ea489d519f511dc71da
Vũ Thanh Tùng <tungvt@relipasoft.com>
2022-05-05 13:44:44 +0700

    Feature/tungvt admin add file

--- layouts/Home.js
+++ layouts/Home.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import Script from 'next/script'
 import Footer from '../components/Footer/HomeFooter'
 import Header from '../components/Header/Header'
-import Head from 'next/head'
 import { MainJS } from '../public/user-page/js/main'
-import { useRouter } from 'next/router'
+import LoadingPages from '../components/HomePage/LoadingPage/LoadingPage'
 
 export default function Home({ children, ...rest }) {
   const { route } = useRouter()
@@ -26,18 +28,19 @@ export default function Home({ children, ...rest }) {
         <link rel='stylesheet' href='/user-page/css/swiper-bundle.min.css' />
         <link rel='stylesheet' href='/user-page/css/template.css' />
       </Head>
+      <LoadingPages />
       <div id='wrapper'>
         <Header />
         <div>{children}</div>
         <Footer />
-        <script src='/user-page/js/popper.min.js' />
-        <script src='/user-page/js/bootstrap.js' />
+        <Script src='/user-page/js/popper.min.js' strategy="beforeInteractive"/>
+        <Script src='/user-page/js/bootstrap.js' strategy="beforeInteractive"/>
 
-        <script src='/user-page/js/isotope.pkgd.min.js' />
-        <script src='/user-page/js/swiper-bundle.min.js' />
-        <script src='/user-page/js/aos.js' />
+        <Script src='/user-page/js/isotope.pkgd.min.js' strategy="beforeInteractive"/>
+        <Script src='/user-page/js/swiper-bundle.min.js' strategy="beforeInteractive"/>
+        <Script src='/user-page/js/aos.js' strategy="beforeInteractive"/>
 
-        <script src='/user-page/js/isotop-filter.js' />
+        <Script src='/user-page/js/isotop-filter.js' strategy="beforeInteractive"/>
       </div>
     </>
   )
