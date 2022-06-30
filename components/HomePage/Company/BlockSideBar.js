54f09a681380f983472ec1ef71f32405e23b8e8a
tungvt <tungvt@relipasoft.com>
2022-06-30 16:58:13 +0700

    Fix link scroll, css sidebar

--- components/HomePage/Company/BlockSideBar.js
+++ components/HomePage/Company/BlockSideBar.js
@@ -4,15 +4,21 @@ const BlockSideBar = () => {
   const trans = useTrans()
   const language = trans.company.company.SideBar
   return (
-    <div className="col-lg-2 d-none d-lg-block">
-      <section className="section section-aos" data-aos="fade-up">
+    <div className="col-lg-2 d-none d-lg-block side-bar-wrap">
+      <section className="section section-aos side-bar" data-aos="fade-up">
         <div className="navigator-section">
           <a className="active" href="#company">
             {language.Company_Profile}
           </a>
-          <a href="#message">{language.CEO_Message}</a>
-          <a href="#mission">{language.MissionValues}</a>
-          <a href="#core-member">{language.Core_Members}</a>
+          <a className="active" href="#message">
+            {language.CEO_Message}
+          </a>
+          <a className="active" href="#mission">
+            {language.MissionValues}
+          </a>
+          <a className="active" href="#core-member">
+            {language.Core_Members}
+          </a>
         </div>
       </section>
     </div>
