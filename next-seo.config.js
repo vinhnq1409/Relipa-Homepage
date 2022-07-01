caf8196741ebf3163210383091eb16248f0484b8
VietTran <viettt@relipasoft.com>
2022-07-01 16:54:38 +0700

    Update SEO title

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,13 +1,13 @@
 export default {
   title: 'Blockchain and Software Development | Relipa',
-  description: 'This is description',
+  description: 'Relipa provides excellent IT outsourcing services in Blockchain Development Services - Web Mobile Application - White Label Blockchain Solutions',
   openGraph: {
     type: 'website',
     locale: 'en',
     url: 'https://relipa.global/',
     images: [
       {
-        url: '/user-page/img/relipa-behind-your-success.png',
+        url: 'https://relipa.global/user-page/img/relipa-behind-your-success.png',
         width: 1200,
         height: 630,
         type: 'image/png',
