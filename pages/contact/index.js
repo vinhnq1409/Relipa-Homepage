2c3a99acfb726ad8928d691b96735294c505d1b9
VietTran <viettt@relipasoft.com>
2022-07-04 10:08:42 +0700

    Update meta tag

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -30,32 +30,8 @@ const Contact = () => {
   return (
     <>
       <NextSeo
-        title="Contact | Relipa"
-        description="This example uses more of the available config options."
-        canonical="https://www.canonical.ie/"
-        openGraph={{
-          title: 'Open Graph Title',
-          description: 'Open Graph Description',
-          images: [
-            {
-              url: 'https://www.example.ie/og-image-01.jpg',
-              width: 800,
-              height: 600,
-              alt: 'Og Image Alt',
-              type: 'image/jpeg',
-            },
-            {
-              url: 'https://www.example.ie/og-image-02.jpg',
-              width: 900,
-              height: 800,
-              alt: 'Og Image Alt Second',
-              type: 'image/jpeg',
-            },
-            { url: 'https://www.example.ie/og-image-03.jpg' },
-            { url: 'https://www.example.ie/og-image-04.jpg' },
-          ],
-          site_name: 'SiteName',
-        }}
+        title="Blockchain and Software Development | Contact RELIPA"
+        description="Please feel free to contact us via the contact form. It may take some days to reply depend on your detailed requirements."
       />
       <HomePage>
         <BlockBanner />
