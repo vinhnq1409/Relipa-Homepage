58352d80cb5cd66d39e4aeda53e67f79180d8555
VietTran <viettt@relipasoft.com>
2022-07-07 17:17:44 +0700

    Refactor code

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import App from 'next/app'
-import Router from 'next/router'
-import store from '../redux/store'
 import { QueryClient, QueryClientProvider } from 'react-query'
-import PageChange from 'components/PageChange/PageChange.js'
 import { Provider } from 'react-redux'
 import Script from 'next/script'
 import { DefaultSeo } from 'next-seo'
+import store from '../redux/store'
 import SEO from '../next-seo.config'
 
 const queryClient = new QueryClient()
-
-// Router.events.on('routeChangeStart', (url) => {
-//   document.body.classList.add('body-page-transition')
-//   ReactDOM.render(<PageChange />, document.getElementById('page-transition'))
-// })
-
-// Router.events.on('routeChangeComplete', () => {
-//   ReactDOM.unmountComponentAtNode(document.getElementById('page-transition'))
-//   document.body.classList.remove('body-page-transition')
-// })
-
-// Router.events.on('routeChangeError', () => {
-//   ReactDOM.unmountComponentAtNode(document.getElementById('page-transition'))
-//   document.body.classList.remove('body-page-transition')
-// })
-
 export default class MyApp extends App {
   static async getInitialProps({ Component, router, ctx }) {
     let pageProps = {}
