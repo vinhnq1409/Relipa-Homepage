6612d6415a91169c5b077fbf9c87a606eb35cf10
VietTran <viettt@relipasoft.com>
2022-11-03 16:43:20 +0700

    Change contact image

--- components/LinkContact/index.js
+++ components/LinkContact/index.js
@@ -13,9 +13,9 @@ const LinkContact = () => {
             <div className="main-banner-item main-banner-item-sm img-banner-contact">
               <div className="main-banner-item-bg background-img-contact ">
                 <picture>
-                  <source media="(min-width:768px)" srcSet="user-page/img/contact.jpeg" />
-                  <source media="(min-width:768px)" srcSet="user-page/img/contact.jpeg" />
-                  <img src="user-page/img/contact.jpeg" width="345" height="188" alt="banner company" />
+                  <source media="(min-width:768px)" srcSet="/user-page/img/contact.jpeg" />
+                  <source media="(min-width:768px)" srcSet="/user-page/img/contact.jpeg" />
+                  <img src="/user-page/img/contact.jpeg" width="345" height="188" alt="banner company" />
                 </picture>
               </div>
               <div className="container ">
